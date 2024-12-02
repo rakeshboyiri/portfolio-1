21127cbc63b8a914ce97fb336d8bc4b2dbc35ef7
= <boyirirakesh.com>
2024-12-02 18:30:18 +0530

    Header

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -3,14 +3,10 @@ import { WavyBackgroundDemo } from './WavyBackgroundDemo'
 
 const Hero = () => {
   return (
-    <section className="min-h-screen flex items-center justify-center text-center w-[100%]">
-      <div className="max-w-3xl mx-auto px-4">
+    <section className="min-h-screen flex items-center justify-center text-center w-full">
+      <div >
         {/* Move WavyBackgroundDemo containing the image above the text */}
-        <WavyBackgroundDemo />
-       
-        
-        
-       
+        <WavyBackgroundDemo />      
       </div>
     </section>
   )
